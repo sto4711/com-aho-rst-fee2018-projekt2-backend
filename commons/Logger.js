0d8ece6400c31933efc74dea3bcf4362b0994258
Roland Stauffer <rstauffe@hsr.ch>
2018-08-14 09:06:55 +0200

    bla

--- commons/Logger.js
+++ commons/Logger.js
@@ -1,11 +1,17 @@
-export class Logger {
+import dateformat from 'dateformat';
+import express from "express";
+import {ControllerAuthentication} from "../controller/ControllerAuthentication";
+import {ControllerProduct} from "../controller/ControllerProduct";
+import authentication from "express-authentication";
 
+
+export class Logger {
     static buildMessage(className, functionName, message) {
         var str = className + '.' + functionName + '()' ;
         while (str.length < 60) {
             str += " ";
         }
-        return str + message;
+        return dateformat(new Date(), "yyyy.mm.dd h:MM:ss TT") + str + message;
     }
 
     static traceMessage(className, functionName, message) {
