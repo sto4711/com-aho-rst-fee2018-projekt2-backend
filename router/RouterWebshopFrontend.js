4ab6bf73aa1c8cb16d8e8b3ce2b13f6ccffeabc1
Roland Stauffer <rstauffe@hsr.ch>
2018-09-25 11:45:32 +0200

    - frontend included: http://localhost:3000/frontend

--- router/RouterWebshopFrontend.js
+++ router/RouterWebshopFrontend.js
@@ -7,15 +7,8 @@ export class RouterWebshopFrontend {
 
         // catch 404 not found when frontend refresh page
         this.router.get('/*',  async (request, response) => {
-            debugger;
-            console.log('request.url ' + request.url + '' + path );
-
-            debugger;
             response.sendFile(path.resolve('public/frontend/index.html'));
-            //response.sendfile(__dirname+'/../public/frontend/index.html');
-            //response.sendFile('public/frontend/index.html', { root: __dirname });
         });
-
     }
 
     getRouter() {
