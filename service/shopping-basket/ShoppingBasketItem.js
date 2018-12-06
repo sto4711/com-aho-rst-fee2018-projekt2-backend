c4d46b152a48a27b0a08c4bdce76eb3d608a757b
Alain <alain.holzhauser@mmp.htwchur.ch>
2018-12-06 15:29:38 +0100

    - shopping basket article link

--- service/shopping-basket/ShoppingBasketItem.js
+++ service/shopping-basket/ShoppingBasketItem.js
@@ -1,7 +1,8 @@
 export class ShoppingBasketItem {
-    constructor(articleID, articleName,articlePrice,imageURL,articleAvailability,articleAmount,itemNumber,articleQueryParameter) {
+    constructor(articleID, articleName,articleBrand, articlePrice,imageURL,articleAvailability,articleAmount,itemNumber,articleQueryParameter) {
         this.articleID = articleID;
         this.articleName = articleName;
+        this.articleBrand = articleBrand;
         this.articlePrice = articlePrice;
         this.imageURL = imageURL;
         this.articleAvailability = articleAvailability;
